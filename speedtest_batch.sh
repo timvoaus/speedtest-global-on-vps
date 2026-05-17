529ea21cd1a1fa929e7f4d37cbb0f3662abd288c
timvoaus <146335301+timvoaus@users.noreply.github.com>
2026-05-17 15:31:24 +1000

    Update speedtest_batch.sh

--- speedtest_batch.sh
+++ speedtest_batch.sh
@@ -37,8 +37,8 @@ min_sleep=450
 max_sleep=900
 
 # Telegram config
-TELEGRAM_BOT_TOKEN="8781656242:AAHk6ZgAADgoCgwzRsKPqDkgtDo_kRjrDto"
-TELEGRAM_CHAT_ID="7381939387"
+TELEGRAM_BOT_TOKEN="YOUR_BOT_TOKEN"
+TELEGRAM_CHAT_ID="YOUR_CHAT_ID"
 
 # Log file
 log_file="./speedtest_log.txt"
